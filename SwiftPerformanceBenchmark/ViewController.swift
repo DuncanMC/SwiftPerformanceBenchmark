77b0f3d705dabcf494e21b35c7e6a14a5dc7c534
Duncan Champney <DuncanC@aol.com>
2015-03-31 08:35:28 -0400

    Initial Commit

---
+++ SwiftPerformanceBenchmark/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftPerformanceBenchmark
+//
+//  Created by Duncan Champney on 3/31/15.
+//  Copyright (c) 2015 Duncan Champney. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
