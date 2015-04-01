9f41cff5762fd88c8b0dfbb867d9a8c47be516c9
Duncan Champney <DuncanC@aol.com>
2015-04-01 10:31:29 -0400

    Added .gitignore file

---
+++ CalcPrimesSwift.swift
@@ -0,0 +1,9 @@
+//
+//  CalcPrimesSwift.swift
+//  SwiftPerformanceBenchmark
+//
+//  Created by Duncan Champney on 4/1/15.
+//  Copyright (c) 2015 Duncan Champney. All rights reserved.
+//
+
+import Foundation
---
+++ SwiftPerformanceBenchmark/CalcPrimesProtocol.h
@@ -0,0 +1,13 @@
+//
+//  CalcPrimesProtocol.h
+//  SwiftPerformanceBenchmark
+//
+//  Created by Duncan Champney on 4/1/15.
+//  Copyright (c) 2015 Duncan Champney. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol CalcPrimesProtocol <NSObject>
+
+@end
